e5d33a176c6e750119525f34d82594a40326bcb2
vincentgrassin <grassinvincent@gmail.com>
2021-10-03 13:29:21 +0200

    implement picture mutations

--- src/entities/objectType.ts
+++ src/entities/objectType.ts
@@ -1,6 +1,15 @@
 import { Roll } from "../entities/Roll";
 import { User } from "../entities/User";
-import { Field, ObjectType } from "type-graphql";
+import { Field, Int, ObjectType } from "type-graphql";
+import { Picture } from "./Picture";
+
+@ObjectType()
+export class PaginatedPicturesByRoll {
+  @Field(() => [Picture])
+  pictures: Picture[];
+  @Field(() => Int)
+  count: number;
+}
 
 @ObjectType()
 export class InvitationRollType {
--- src/resolvers/pictureResolver.ts
+++ src/resolvers/pictureResolver.ts
@@ -1,11 +1,19 @@
 import { Participant } from "../entities/Participant";
-import { Resolver, Arg, Mutation, UseMiddleware, Ctx } from "type-graphql";
+import {
+  Resolver,
+  Arg,
+  Mutation,
+  UseMiddleware,
+  Ctx,
+  Query,
+} from "type-graphql";
 import { createQueryBuilder } from "typeorm";
 import { Picture } from "../entities/Picture";
 import { Roll } from "../entities/Roll";
 import { errorMessages } from "../constants";
 import { isAuth } from "../isAuth";
-import { MyContext } from "src/MyContext";
+import { MyContext } from "../MyContext";
+import { PaginatedPicturesByRoll } from "../entities/objectType";
 
 @Resolver()
 export class PictureResolver {
@@ -41,5 +49,46 @@ export class PictureResolver {
     } else {
       throw new Error(errorMessages.pictureUploadFailed);
     }
+  }
+
+  @Query(() => [Picture])
+  @UseMiddleware(isAuth)
+  async getPicturesByRoll(
+    @Arg("rollId") rollId: number,
+    @Ctx() { payload }: MyContext
+  ): Promise<(Picture | undefined)[]> {
+    if (!payload) {
+      throw new Error(errorMessages.unabledToFind);
+    }
+    const pictures = await createQueryBuilder("picture")
+      .select("picture")
+      .from(Picture, "picture")
+      .where("picture.rollId = :rollId", { rollId })
+      .getMany();
+
+    return pictures;
+  }
+
+  @Query(() => PaginatedPicturesByRoll)
+  @UseMiddleware(isAuth)
+  async getPaginatedPicturesByRoll(
+    @Arg("rollId") rollId: number,
+    @Arg("limit") limit: number,
+    @Arg("offset") offset: number,
+    @Ctx() { payload }: MyContext
+  ): Promise<{ count: number; pictures: Picture[] | undefined }> {
+    if (!payload) {
+      throw new Error(errorMessages.unabledToFind);
+    }
+    const pictures = await createQueryBuilder("picture")
+      .select("picture")
+      .distinct(true)
+      .from(Picture, "picture")
+      .where("picture.rollId = :rollId", { rollId })
+      .skip(offset)
+      .take(limit)
+      .getMany();
+
+    return { pictures, count: pictures.length };
   }
 }
