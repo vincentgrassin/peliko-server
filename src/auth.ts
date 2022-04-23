878efe0622cda2e5fa463d964eab8596c6517f18
Vincent Grassin <vgrassin@talentsoft.com>
2022-04-23 14:00:28 +0200

    fix : set longer accesstoken

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import { User } from "./entities/User";
 
 export const createAccessToken = (user: User) => {
   return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: "15m",
+    expiresIn: "1h",
   });
 };
 
